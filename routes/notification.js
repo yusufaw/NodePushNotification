44bc32c29ea0ebb2a284af29bc19ba9654c2d0cd
Yusuf Aji Wibowo <yusuf@skyshi.com>
2017-05-12 22:09:04 +0700

    Add firebase config json

--- routes/notification.js
+++ routes/notification.js
@@ -2,6 +2,22 @@ const express = require('express');
 const router = express.Router();
 const request = require('request');
 const config = require('../.config');
+const admin = require('firebase-admin');
+var registrationToken = config.firebaseServerKey;
+
+var serviceAccount = require("../firebase-config.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount)
+});
+
+var payload = {
+    data: {
+        "title": "halo semuanya",
+        "body": "hehe"
+    }
+};
+
 
 router.get('/', (req, res, next) => {
     request({
@@ -24,9 +40,23 @@ router.get('/', (req, res, next) => {
         } else if (response.statusCode >= 400) {
             res.send('HTTP Error: ' + response.statusCode + ' - ' + response.statusMessage + '\n' + body);
         } else {
+            console.log(response);
             res.send('Done!')
         }
     });
+});
+
+router.get('/oke', (req, res, next) => {
+    admin.messaging().sendToDevice('', payload)
+        .then(function(response) {
+            // See the MessagingDevicesResponse reference documentation for
+            // the contents of response.
+            console.log("Successfully sent message:", response);
+            res.send('')
+        })
+        .catch(function(error) {
+            console.log("Error sending message:", error);
+        });
 });
 
 module.exports = router;
